255ec31432571b300bcf6bef4516b9206562ae37
arijitdeb1502 <arijitdeb.git@gmail.com>
2020-02-23 08:13:09 +0800

    Added Header style to header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, {Component} from 'react';
+ import React, { Component } from 'react';
+ import Header from './Header';
 
 class App extends Component {
-  
-  render(){
+
+  render() {
     return (
       <div>
+        <Header/>
       </div>
     )
   }
 }
 
 export default App;
+ 
