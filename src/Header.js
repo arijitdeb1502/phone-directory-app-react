d6ca4a2c738addca8442aaeee39d1b85287684a6
arijitdeb1502 <arijitdeb.git@gmail.com>
2020-02-23 08:20:38 +0800

    Added external styling for header

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import './Header.css';
 
 const Header = ()=>{
     
-    const headerStyle = {
-        textAlign: 'center',
-        padding: 20,
-        background: '#000',
-        color: '#fff',
-        textTransform: 'uppercase'
-    };
+    // const headerStyle = {
+    //     textAlign: 'center',
+    //     padding: 20,
+    //     background: '#000',
+    //     color: '#fff',
+    //     textTransform: 'uppercase'
+    // };
 
     return(
-        <div style={headerStyle}>
+        <div className='header'>
             Phone Directory
         </div>
     )
